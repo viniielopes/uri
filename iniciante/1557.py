ecfaa8cdb5e459ce280652bc91bc3036ed674456
Vinicius Lopes <viniielopes@live.com>
2021-07-05 13:55:16 -0300

    feat: add comentario

--- iniciante/1557.py
+++ iniciante/1557.py
@@ -10,22 +10,22 @@ while valor != 0:
   padding = 0
 
   coluna = 1
+  # contabiliza e verifica qual é o ultimo numero e define o padding
   for i in range(1, valor + 1):
 
     linha = coluna
     for j in range(1, valor + 1):
-
-
       if i == valor and j == valor:
         padding = len(str((linha))) + 1
-        # print(padding)
 
       linha = linha * 2
     coluna = coluna * 2
    
+  # contabiliza e exibe os numeros utilizando padding definido
   contlinha = 1
   contcoluna = 1
   for i in range(1, valor + 1):
+
     contlinha = contcoluna
     for j in range(1, valor + 1):
 
@@ -33,7 +33,7 @@ while valor != 0:
         print("{:{padding}d}".format(contlinha, padding=padding - 1), end='')
       else:
         print("{:{padding}d}".format(contlinha, padding=padding), end='')
-     
+
       contlinha = contlinha * 2
     contcoluna = contcoluna * 2
     print()
